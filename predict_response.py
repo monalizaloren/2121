1cf05c034aa1ed939985c78e61fcd4ee4732a69c
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-08-14 15:58:04 -0300

    Update predict_response.py

--- predict_response.py
+++ predict_response.py
@@ -1,13 +1,11 @@
-# Bibliotecas de pré-processamento de dados de texto
 import nltk
-
 import json
 import pickle
 import numpy as np
 import random
 
-ignore_words = ["?","!",".",","]
-
+# Adicionei uma lista de palavras ignoradas para limpeza
+ignore_words = ["?", "!", ".", ","]
 
 # Biblioteca load_model
 import tensorflow
@@ -17,28 +15,29 @@ model = tensorflow.keras.models.load_model('./chatbot_model.h5')
 
 # Carregue os arquivos de dados
 intents = json.loads(open('./intents.json').read())
-words = pickle.load(open('./words.pkl','rb'))
-classes = pickle.load(open('./classes.pkl','rb'))
+words = pickle.load(open('./words.pkl', 'rb'))
+classes = pickle.load(open('./classes.pkl', 'rb'))
 
 
 def preprocess_user_input(user_input):
 
     input_word_token_1 = nltk.word_tokenize(user_input)
-    input_word_token_2 = get_stem_words(input_word_token_1, ignore_words) 
+    input_word_token_2 = get_stem_words(input_word_token_1, ignore_words)
     input_word_token_2 = sorted(list(set(input_word_token_2)))
 
-    bag=[]
+    bag = []
     bag_of_words = []
-   
-    # Codificação dos dados de entrada 
-    for word in words:            
-        if word in input_word_token_2:              
+
+    # Codificação dos dados de entrada
+    for word in words:
+        if word in input_word_token_2:
             bag_of_words.append(1)
         else:
-            bag_of_words.append(0) 
+            bag_of_words.append(0)
     bag.append(bag_of_words)
-  
+
     return np.array(bag)
+
 
 def bot_class_prediction(user_input):
 
@@ -47,20 +46,23 @@ def bot_class_prediction(user_input):
     predicted_class_label = np.argmax(prediction[0])
     return predicted_class_label
 
+
 def bot_response(user_input):
 
     predicted_class_label = bot_class_prediction(user_input)
     predicted_class = classes[predicted_class_label]
 
     for intent in intents["intents"]:
-     if intent['tag'] == predicted_class:
-        bot_response = random.choice(intent["response"])
-        return bot_response
-print("oi,sou a estrela, como posso ajudar") 
+        if intent['tag'] == predicted_class:
+            bot_response = random.choice(intent["responses"])
+            return bot_response
+
+
+print("Oi, sou a Estrela. Como posso ajudar?")
 
 while True:
     user_input = input("Digite sua mensagem aqui:")
-    print("entrada do usuarioc ", user_input)
+    print("Entrada do usuário:", user_input)
 
     response = bot_response(user_input)
-    print("resposta do robo ", response)
+    print("Resposta do robô:", response)
